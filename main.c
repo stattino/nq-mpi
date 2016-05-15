bc5c8fa0c91f945f6dba0ca9c253874dc59ffd73
Oskar Stattin <stattin.o@gmail.com>
2016-05-15 19:45:50 +0200

    removing std::

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-#include <vector>
-#include <iostream>
+#include <stdlib.h>
 // #include <mpi.h>
-static const int BOARDSIZE=6;
+static const int BOARDSIZE=7;
 static int solutions=0;
 
 
 static bool threatens(const int row_1,const int row_2,const int col_1,const int col_2) {
     //  Feels pretty done
     //  Checks if the queens are on the same row, or diagonal
-    if ( row_1==row_2||std::abs(row_1-row_2)==std::abs(col_1-col_2) )return true;
-    else return false;
+    if ( row_1==row_2||abs(row_1-row_2)==abs(col_1-col_2) ){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-static bool is_checked(const int counter, std::vector<int> board) {
+static bool is_checked(const int counter, int board[]) {
     // parallelize this
     // Redundant checking ATM.. to be fixed
     int row_1, row_2;
@@ -23,15 +26,15 @@ static bool is_checked(const int counter, std::vector<int> board) {
         // second loop over following columns. no need checking the same cols/rows
         for (int col_2=col_1+1; col_2<counter; col_2++) {
             row_2 = board[col_2];
-            if (threatens(row_1, row_2, col_1, col_2)) return false;
+            if (threatens(row_1, row_2, col_1, col_2)) {return false;}
         }
     }
     return true;
 }
 
-static void nq_recursion(int counter, std::vector<int> board) {
-    // row = number in the std::vector
-    // col = index in std::vector
+static void nq_recursion(const int counter, int board[]) {
+    // row = number in the board vector
+    // col = index in board vector
     // counter decides which column we are in. switch name mbe...
     
     // End condition, counter is boardsize and no threats: add a solution
@@ -59,9 +62,10 @@ static void nq_recursion(int counter, std::vector<int> board) {
 
 
 int main(int argc, char *argv[]) {
-    std::vector<int> chessboard(BOARDSIZE);
+    // std::vector<int> chessboard(BOARDSIZE);
+    int chessboard[BOARDSIZE];
     nq_recursion(0, chessboard);
-    std::cout << "Solutions: " << solutions << " \n" ;
+    printf("Solutions: %i", solutions);
     
     /*
      int myrank, mysize;
@@ -70,6 +74,8 @@ int main(int argc, char *argv[]) {
      MPI_Init(&argc, &argv);
      MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
      MPI_Comm_size(MPI_COMM_WORLD, &mysize);
+     
+     MPI_Comm_spawn()
      
      MPI_Finalize();*/
 }
