778f7eb8d24d1ea97fe0f26b3b6fb4f63f4fec32
Oskar Stattin <stattin.o@gmail.com>
2016-06-04 16:23:26 +0200

    changed timing, boardsize is now input

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <mpi.h>
 #include <time.h>
-static const int BOARDSIZE=14;
+//static const int BOARDSIZE=14;
 static int solutions=0;
 typedef int bool;
 #define true 1
 #define false 0
 
 static bool threatens(const int row_1,const int row_2,const int col_1,const int col_2) {
-    //  Feels pretty done
     //  Checks if the queens are on the same row, or diagonal
-    if ( row_1==row_2||abs(row_1-row_2)==abs(col_1-col_2) ){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return (row_1==row_2||abs(row_1-row_2)==abs(col_1-col_2)) ? true:false;
 }
 
 static bool is_checked(const int counter, int board[]) {
@@ -35,13 +28,12 @@ static bool is_checked(const int counter, int board[]) {
     return true;
 }
 
-static void nq_recursion(const int counter, int board[]) {
+static void nq_recursion(const int counter, int board[], int boardSize) {
     // row = number in the board vector
     // col = index in board vector
-    // counter decides which column we are in. switch name mbe...
-    
+    // counter = which column is "active"
     // End condition, counter is boardsize and no threats: add a solution
-    if (counter >= BOARDSIZE) {
+    if (counter >= boardSize) {
         if (is_checked(counter,board)) {
             solutions++;
         }
@@ -56,35 +48,24 @@ static void nq_recursion(const int counter, int board[]) {
     // Recursion over all the new row-positions for this col
     // This is to be parallelized
     else{
-        for(int row=0; row<BOARDSIZE; row++){
+        for(int row=0; row<boardSize; row++){
             board[counter] = row;
-            nq_recursion(counter+1, board);
+            nq_recursion(counter+1, board, boardSize);
         }
     }
 }
 
-
 int main(int argc, char *argv[]) {
-    // std::vector<int> chessboard(BOARDSIZE);
-    int chessboard[BOARDSIZE];
-    clock_t time;
-    time = clock();
-    nq_recursion(0, chessboard);
-    time = clock() - time;
-    double elapsed_time = ((double)(time))/CLOCKS_PER_SEC;
-    printf("Solutions: %i in %f seconde", solutions, elapsed_time);
+    int boardSize;
+    (argc<2) ? boardSize=10 : sscanf(argv[1],"%d",&boardSize);
+    //printf("Input: %d", N);
     
-    /*
-     int myrank, mysize;
-     MPI_Status status;
-     
-     MPI_Init(&argc, &argv);
-     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-     MPI_Comm_size(MPI_COMM_WORLD, &mysize);
-     
-     MPI_Comm_spawn()
-     
-     MPI_Finalize();*/
+    int chessboard[boardSize];
+    time_t tStart;
+    tStart = time(NULL);
+    nq_recursion(0, chessboard, boardSize);
+    tStart = time(NULL) - tStart;
+    printf("Solutions: %d in %f seconds", solutions, ((double)(tStart)));
 }
 
 
